9afe5977f4c0433acd6a037a2b5543c92904e4d4
suhdanny <suhdong21@gmail.com>
2022-07-15 17:23:59 -0400

    Implement AuthContext for storing user information

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+	const [loading, setLoading] = useState(true);
+	const [user, setUser] = useState({});
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		onAuthStateChanged(auth, user => {
+			setUser(user);
+			setLoading(false);
+			navigate('/chats');
+		});
+	}, [user, navigate]);
+
+	const value = {
+		user,
+	};
+
+	return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
+};
